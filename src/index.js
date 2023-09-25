75c8eafd06749782fae417c2772314813c871f37
Prerit <preritmishra1002@gmail.com>
2023-09-25 16:53:42 +0530

    Started adding create service for tweets

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
+const Tweet = require('./models/tweet');
+
 app.listen(3000, async()=> {
     console.log("Servr started");
     await connect();
-    console.log("Mongodb connected")
+    console.log("Mongodb connected");
+    const tweets = await Tweet.find({
+        content: ["First Tweet", "Third Tweet", "123m4"]
+    });
+    console.log(tweets); 
 });
