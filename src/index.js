34c6aa3e9e9c72de96ffb6f526818ab9d74237b8
Prerit <preritmishra1002@gmail.com>
2023-09-27 14:09:24 +0530

    Moved to es module

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-const express = require('express');
-const connect = require('./config/database');
+import express from 'express';
+import {connect} from './config/database.js';
 const app = express();
 
-const {TweetRepository} = require('./repository/index');
-const TweetService = require('./services/tweet-service');
+import service from './services/tweet-service.js'
 
 app.listen(3000, async()=> {
-    console.log("Servr started");
+    console.log("Server started");
     await connect();
     console.log("Mongodb connected");
-    let service = new TweetService();
-    const tweet = await service.create({content: 'Is #X working?'});
-    console.log(tweet);
 });
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,4 +1,4 @@
-const { TweetRepository, HashtagRepository } = require('../repository/index');
+import { TweetRepository, HashtagRepository } from '../repository/index.js';
 
 class TweetService {
     constructor() {
@@ -27,6 +27,5 @@ class TweetService {
     }
 }
 
-module.exports = TweetService;
-
+export default TweetService;
  
