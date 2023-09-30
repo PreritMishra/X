e740617e3576d19fa949fe43de60025a0ea68e7e
Prerit <preritmishra1002@gmail.com>
2023-09-30 12:35:33 +0530

    Added toggle like api

---
+++ src/controllers/like-controller.js
@@ -0,0 +1,25 @@
+import LikeService from '../services/like-service.js';
+
+const likeService = new LikeService();
+
+export const toggleLike = async(req,res) => {
+    try {
+        //console.log("body", req.query);
+
+        const response = await likeService.toggleLike(req.query.modelId, req.query.modelType, req.body.userId);
+        return res.status(200).json({
+            success: true,
+            data: response,
+            err: {},
+            message: 'Successfully toggeled the like'
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            data: {},
+            message: 'Something went wrong',
+            err: error
+        });
+    }
+} 
